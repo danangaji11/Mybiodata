25eb8d74773eec141631e9bd90195e692a96d599
danangaji11 <danangajipangestu99@gmail.com>
2023-02-23 11:23:07 +0700

    .

--- app/src/main/java/com/danang/myapplication/MainActivity.java
+++ app/src/main/java/com/danang/myapplication/MainActivity.java
@@ -2,13 +2,44 @@ package com.danang.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private  Utilitis utilitis = new Utilitis();
+    private TextView tvUsername;
+    private Button btnlogout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvUsername = findViewById(R.id.tv_username);
+        btnlogout = findViewById(R.id.btn_logout);
+
+        if (!utilitis.isLogin(this, "xUsername")){
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }else{
+            String username = utilitis.getPref(this, "xUsernames");
+            tvUsername.setText(username);
+        }
+
+        btnlogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                utilitis.setPref(MainActivity.this, "xUsername", null);
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
